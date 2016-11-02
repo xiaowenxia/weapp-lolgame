03fdd11d2db115a5575c213bf2266614a88dca15
xiaowen.xia <775117471@qq.com>
2016-11-02 22:51:56 +0800

    增加视频预览页面

--- pages/userinfo/battledetail/battledetail.js
+++ pages/userinfo/battledetail/battledetail.js
@@ -14,44 +14,7 @@ Page({
       title: "战绩详情"
     })
   },
-  //计算小时数后剩余的毫秒数
-  cal_minute(date1, date2) {
-    var date3 = date2.getTime()-date1.getTime()  //时间差的毫秒数
-    //计算出相差天数
-    var days=Math.floor(date3/(24*3600*1000))
-    //计算出小时数
-    var leave1=date3%(24*3600*1000)    //计算天数后剩余的毫秒数
-    var hours=Math.floor(leave1/(3600*1000))
-    //计算相差分钟数
-    var leave2=leave1%(3600*1000)        //计算小时数后剩余的毫秒数
-    var minutes=Math.floor(leave2/(60*1000))
-    //计算相差秒数
-    var leave3=leave2%(60*1000)      //计算分钟数后剩余的毫秒数
-    var seconds=Math.round(leave3/1000)
-
-    function sprintf(){
-         var as=[].slice.call(arguments),fmt=as.shift(),i=0;
-      return     fmt.replace(/%(\w)?(\d)?([dfsx])/ig,function(_,a,b,c){
-              var s=b?new Array(b-0+1).join(a||''):'';
-              if(c=='d') s+=parseInt(as[i++]);
-              return b?s.slice(b*-1):s;
-         })
-    }
-    var time = {
-      "days": days,
-      "hours": hours,
-      "minutes": minutes,
-      "seconds": seconds
-    }
-    var time = ''
-    if(days)
-      console.log("something wrong, this game time is too long")
-    if(hours)
-      time += hours+":"
-    time += sprintf("%02d",minutes)+":"+sprintf("%02d",seconds)
-    console.log(time)
-    return time
-  },
+  
   detailTap: function(event) {
     
     var idx = event.currentTarget.dataset.index
@@ -96,8 +59,23 @@ Page({
         /* 计算游戏时间 */
         var date1 = new Date(res.data.data[0].battle.start_time.replace(/-/g,"/"));
         var date2 = new Date(res.data.data[0].battle.stop_time.replace(/-/g,"/"));
-        var time_diff = that.cal_minute(date1, date2)
+        var time_diff = date.TimeDiff(date1, date2)
 
+        function sprintf(){
+          var as=[].slice.call(arguments),fmt=as.shift(),i=0;
+          return     fmt.replace(/%(\w)?(\d)?([dfsx])/ig,function(_,a,b,c){
+                  var s=b?new Array(b-0+1).join(a||''):'';
+                  if(c=='d') s+=parseInt(as[i++]);
+                  return b?s.slice(b*-1):s;
+             })
+        }
+        var time = ''
+        if(time_diff.days)
+          console.log("something wrong, this game time is too long")
+        if(time_diff.hours)
+          time += time_diff.hours+":"
+        time += sprintf("%02d",time_diff.minutes)+":"+sprintf("%02d",time_diff.seconds)
+        console.log(time)
         /* 计算KDA总数据 */
         var win_cnt = 0,lost_cnt = 0
         var detail = res.data.data[0].battle
@@ -128,7 +106,7 @@ Page({
           game_detail: res.data.data[0].battle,
 
           start_time: res.data.data[0].battle.start_time.split(' ')[1],
-          game_time: time_diff,
+          game_time: time,
           win: win,
           lost: lost,
           win_cnt: win_cnt,
--- utils/util.js
+++ utils/util.js
@@ -31,7 +31,30 @@ function stringToDate(str){
     return date;
 }
 
+//计算小时数后剩余的毫秒数
+function TimeDiff(date1, date2) {
+    var date3 = date2.getTime()-date1.getTime()  //时间差的毫秒数
+    //计算出相差天数
+    var days=Math.floor(date3/(24*3600*1000))
+    //计算出小时数
+    var leave1=date3%(24*3600*1000)    //计算天数后剩余的毫秒数
+    var hours=Math.floor(leave1/(3600*1000))
+    //计算相差分钟数
+    var leave2=leave1%(3600*1000)        //计算小时数后剩余的毫秒数
+    var minutes=Math.floor(leave2/(60*1000))
+    //计算相差秒数
+    var leave3=leave2%(60*1000)      //计算分钟数后剩余的毫秒数
+    var seconds=Math.round(leave3/1000)
+    var time = {
+      "days": days,
+      "hours": hours,
+      "minutes": minutes,
+      "seconds": seconds
+    }
+    return time
+}
 module.exports = {
   formatTime: formatTime,
-  stringToDate: stringToDate
+  stringToDate: stringToDate,
+  TimeDiff: TimeDiff
 }
